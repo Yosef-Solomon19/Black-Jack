e54ad5ffe0f9549c3d1ccb5af9ca4838bb84ab4e
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-07-26 14:05:39 -0400

    Adding comments

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ let sum = 0;
 let cards = [];
 let isAlive = false; 
 let message = ""; 
-let messageEl= document.getElementById("message-el") ; 
-let sumEl = document.getElementById("sum-el") ; 
-let cardsEl = document.getElementById("cards-el") ; 
+let messageEl= document.getElementById("message-el") ; // Creating a variable that is selecting the element by its id "message-el". 
+                                                       // We are going to be modifying the content inside the message-el paragraph
+let sumEl = document.getElementById("sum-el") ;     // Creating a variable that is selecting the element by its id "sum-el".
+let cardsEl = document.getElementById("cards-el") ; // Creating a variable that is selecting the element by its id "cards-el".
+                                                    // We're doing this to display the new card values to the user to see what the user got.
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
