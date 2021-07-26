6e20c5fb47d29b1ba438a0d9e1bfe22c9475d58f
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-07-26 14:02:37 -0400

    Adding getRandomCard function

--- index.js
+++ index.js
@@ -9,4 +9,13 @@ let messageEl= document.getElementById("message-el") ;
 let sumEl = document.getElementById("sum-el") ; 
 let cardsEl = document.getElementById("cards-el") ; 
 
-
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1 ) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
