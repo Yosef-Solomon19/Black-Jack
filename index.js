8853b8c4711dfa63a3f5d9b41cf2a60c3421c7ee
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-07-26 14:00:26 -0400

    Adding global variables

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let firstCard = 0;
+let secondCard = 0; 
+let sum = 0; 
+let cards = [];
+let isAlive = false; 
+let message = ""; 
+let messageEl= document.getElementById("message-el") ; 
+let sumEl = document.getElementById("sum-el") ; 
+let cardsEl = document.getElementById("cards-el") ; 
+
+
