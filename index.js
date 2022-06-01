66aab740918ff5e4bc9108f14da2cbcbd0f2b225
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-06-01 14:46:08 -0400

    Adding conditional statement for sum result

--- index.js
+++ index.js
@@ -1,68 +1,13 @@
+'use strict';
 
-// let firstCard = 0;
-// let secondCard = 0; 
-// let sum = 0; 
-// let cards = [];
-// let isAlive = false; 
-// let hasBlackJack = false;
-// let message = ""; 
-// let messageEl= document.getElementById("message-el") ; // Creating a variable that is selecting the element by its id "message-el". 
-//                                                        // We are going to be modifying the content inside the message-el paragraph
-// let sumEl = document.getElementById("sum-el") ;     // Creating a variable that is selecting the element by its id "sum-el".
-// let cardsEl = document.getElementById("cards-el") ; // Creating a variable that is selecting the element by its id "cards-el".
-//                                                     // We're doing this to display the new card values to the user to see what the user got.
+let firstCard = 10;
+let secondCard = 11; 
+let sum = firstCard + secondCard; 
 
-// function getRandomCard() {
-//     let randomNumber = Math.floor(Math.random() * 13) + 1
-//     if (randomNumber > 10) {
-//         return 10
-//     } else if (randomNumber === 1 ) {
-//         return 11
-//     } else {
-//         return randomNumber
-//     }
-// }
-
-// function startGame() {
-//     isAlive = true;                 
-//     firstCard = getRandomCard()
-//     secondCard = getRandomCard()
-//     sum = firstCard + secondCard 
-//     cards = [firstCard, secondCard]
-//     renderGame()
-// }
-
-// function renderGame() {                         // creating a function to continue the game. 
-//     cardsEl.textContent = "Cards: "             // Selecting the cardsEl to display "Cards:"
-//     for (let i = 0; i < cards.length; i++) {    // For every element that is inside the cards array
-//         cardsEl.textContent += cards[i] + " "   // Add the element that is inside the array by its index provided by the count
-//                                                 // to the cardsEl.textContent till it reaches the maximum number of elements inside the array.
-//                                                 // which is done by comparing the count to the arrays length "cards.length"
-//         }
-
-//         sumEl.textContent = "Sum: " + sum       // Select sumEl to desplay the total sum of the card values.
-//         if (sum <= 20) {
-//             message = "Do you want to draw another card?";
-//         } else if (sum === 21) {
-//             message = "You've got Blackjack!!";
-//             hasBlackJack = True; 
-//         } else {
-//             message = "You're out of the game";
-//             isAlive = false;
-//         }
-//         messageEl.textContent = message; 
-    
-
-// }
-
-// function newCard () {
-//     if (isAlive === true && hasBlackJack === false) { // if the player is still alive AND does not have have blackjack 
-//                                                       // the block of code inside the if statement allows the player to get a new card 
-//                                                      // if these two conditions are met.
-//         let card = getRandomCard() ; 
-//         sum += card
-//         cards.push(card)
-//         renderGame()
-//     }
-    
-// }
+if (sum <= 20) {
+    console.log("Do you want to draw a new card.");
+} else if (sum === 21) {
+    console.log("Sweet! You've got Blackjack! 🥳.");
+} else {
+    console.log("You're out of the game! 😭.");
+}
